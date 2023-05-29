72d248ba3f339762f977a8d7549b9c423fd3fc31
Sumanone8 <sumanjhabhl@gmail.com>
2023-05-29 23:21:40 +0530

    Fibonacii Series program Implemented

---
+++ LogicalPrograming/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number of terms in the Fibonacci series: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci Series:");
+        PrintFibonacciSeries(n);
+    }
+
+    static void PrintFibonacciSeries(int n)
+    {
+        int num1 = 0;
+        int num2 = 1;
+
+        Console.Write($"{num1} {num2} ");
+
+        for (int i = 2; i < n; i++)
+        {
+            int nextNum = num1 + num2;
+            Console.Write($"{nextNum} ");
+
+            num1 = num2;
+            num2 = nextNum;
+        }
+
+        Console.WriteLine();
+    }
+}
