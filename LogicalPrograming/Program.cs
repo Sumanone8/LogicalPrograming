fbd1fe6a877a61aedfa9cb9dd60fd3bbd5a2ecd6
Sumanone8 <sumanjhabhl@gmail.com>
2023-05-29 23:35:54 +0530

    Program for generating distinct coupon numbers Implemeneted

--- LogicalPrograming/Program.cs
+++ LogicalPrograming/Program.cs
@@ -1,29 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter a number: ");
-        int number = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the number of distinct coupon numbers: ");
+        int numCoupons = Convert.ToInt32(Console.ReadLine());
 
-        int reversedNumber = ReverseNumber(number);
+        int totalRandomNumbersNeeded = GenerateDistinctCoupons(numCoupons);
 
-        Console.WriteLine($"The reversed number is: {reversedNumber}");
+        Console.WriteLine($"Total random numbers needed to have all distinct coupons: {totalRandomNumbersNeeded}");
     }
 
-    static int ReverseNumber(int number)
+    static int GenerateDistinctCoupons(int numCoupons)
     {
-        int reversedNumber = 0;
+        Random random = new Random();
+        HashSet<int> distinctCoupons = new HashSet<int>();
+        int totalRandomNumbersNeeded = 0;
 
-        while (number != 0)
+        while (distinctCoupons.Count < numCoupons)
         {
-            int remainder = number % 10;
-            reversedNumber = reversedNumber * 10 + remainder;
-            number /= 10;
+            int randomNumber = random.Next(1, numCoupons + 1);
+
+            if (!distinctCoupons.Contains(randomNumber))
+            {
+                distinctCoupons.Add(randomNumber);
+            }
+
+            totalRandomNumbersNeeded++;
         }
 
-        return reversedNumber;
+        return totalRandomNumbersNeeded;
     }
 }
 
